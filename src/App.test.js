994be0e814202c72cffdeb291f5fd18502379fdd
DanielFarkasEpam <Daniel_Farkas@epam.com>
2017-06-01 22:43:12 +0200

    added test skeletons

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import reducer from './reducer.js';
+
+const store = createStore(reducer);
+
+it('', () => {
+  const component = document.createElement(
+    <div></div>
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store={store}>
+        <App />
+      </Provider>, div);
 });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reducer from './reducer.js'
+import reducer from './reducer.js';
 import thunkMiddleware from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
